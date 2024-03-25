b3034719544c16f284a143edcfb41fe794484a5d
indofreshcoconut <indofreshcoconut@gmail.com>
2024-03-25 19:14:13 +0700

    Signed-off-by: indofreshcoconut <indofreshcoconut@gmail.com>

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+	<engineName>ClassicPress</engineName>
+	<engineLink>https://www.classicpress.net</engineLink>
+	<homePageLink>https://indofreshcoconut.npmpt.com</homePageLink>
+	<apis>
+	  <api name="WordPress" blogID="1" preferred="true" apiLink="https://indofreshcoconut.npmpt.com/xmlrpc.php" />
+	  <api name="Movable Type" blogID="1" preferred="false" apiLink="https://indofreshcoconut.npmpt.com/xmlrpc.php" />
+	  <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://indofreshcoconut.npmpt.com/xmlrpc.php" />
+	  <api name="Blogger" blogID="1" preferred="false" apiLink="https://indofreshcoconut.npmpt.com/xmlrpc.php" />
+	  	<api name="WP-API" blogID="1" preferred="false" apiLink="https://indofreshcoconut.npmpt.com/wp-json/" />
+		</apis>
+  </service>
+</rsd>
+	
